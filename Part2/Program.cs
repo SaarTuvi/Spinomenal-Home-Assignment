13224aa384fc1a8be1723ee0526365b33ad7aee7
Saar Tuvi <saar.tuvi@gmail.com>
2022-02-13 11:12:50 +0200

    Add project files.

---
+++ Part2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int squereHight = 100;
+            int squereWidth = 100;
+            string squereColor = "Green";
+            int spinDegree = 360;
+            int totalFadeTime = 4;
+            int delayTime = 3;
+            int growSquereBy = 4;
+
+            Squere squereObj = new Squere(squereWidth, squereHight, squereColor);
+            //Constructing the squere with dimentions and color required
+            while (spinDegree >= 180)
+            {
+                //Making sure the degree given is 180.
+                ////if it's more, it will spin 180 one time, and reduct 180 from the full spin requried
+                squereObj.AngleVector(spinDegree - 180, 1);
+                spinDegree = spinDegree - 180;
+            }
+
+            squereObj.Fade(totalFadeTime / 2, 100, 0); //Full opacity to minimum in half the fade time (fade out)
+            squereObj.Fade(totalFadeTime / 2, 0, 100); //Minimum opacity to full in half the fade time (fade in)
+            squereObj.Delay(delayTime);
+            squereObj.Grow(growSquereBy, growSquereBy); //Multiply both hight and width with the desired growth
+
+
+            Console.WriteLine(squereObj.ToString()); //The ToString was overriden to meet the task demands
+        }
+    }
+}
---
+++ Part2/Squere.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Part2
+{
+    public class Squere
+    {
+        #region Props
+        public int Width { get; set; }
+        public int Hight { get; set; }
+        public string Color { get; set; }
+        #endregion
+
+        #region Ctors
+        public Squere()
+        {
+
+        }
+
+        public Squere(int width, int hight, string color)
+        {
+            this.Width = width;
+            this.Hight = hight;
+            this.Color = color;
+        }
+
+        #endregion
+
+        #region Functions
+        public void AngleVector(int angle, int x)
+        {
+            //The function rotates the object by ‘angle’ number of degrees, ‘x’ number of times.
+        }
+
+        public void Fade(int duration, int sourceOpacity, int destOpacity)
+        {
+            // The function changes the opacity from ‘sourceOpacity’ to ‘destOpacity’ over ‘duration’
+        }
+
+        public void Grow(int width, int height)
+        {
+            // function that gets new width and height and multiplies the current
+            // width by ‘width’ and the current height by ‘height’.
+        }
+
+        public void Delay(int x)
+        {
+            //   stops the current flow of the code by ‘x’ milliseconds.
+        }
+
+        public override string ToString()
+        {
+            return $"Shape size: {this.Hight} * {this.Width}";
+        }
+
+        #endregion
+    }
+}
